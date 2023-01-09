07e62039fa0a8b75861e25d2082518712f54d5f0
Soe-sandar-win <soesandarwin472000@gmail.com>
2023-01-09 10:49:41 +0630

    middleware section

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +20,16 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [
+    App\Http\Controllers\HomeController::class,
+    'index',
+])->name('home');
+
+Route::prefix('admin')
+    ->middleware(['auth', 'isAdmin'])
+    ->group(function () {
+        Route::get('dashboard', [
+            App\Http\Controllers\Admin\DashboardController::class,
+            'index',
+        ]);
+    });
