e5924155c2900b0fd97ae64202ca4c0d40e056f7
Soe-sandar-win <soesandarwin472000@gmail.com>
2023-01-16 23:33:01 +0630

    add product to wishlist

--- app/Http/Livewire/Frontend/Product/View.php
+++ app/Http/Livewire/Frontend/Product/View.php
@@ -2,11 +2,36 @@
 
 namespace App\Http\Livewire\Frontend\Product;
 
+use App\Models\Wishlist;
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 
 class View extends Component
 {
     public $category, $product, $prodColorSelectedQuantity;
+
+    public function addToWishList($productId)
+    {
+        if (Auth::check()) {
+            if (
+                Wishlist::where('user_id', auth()->user()->id)
+                    ->where('product_id', $productId)
+                    ->exists()
+            ) {
+                session()->flash('message', 'Already added to wishlist');
+                return false;
+            } else {
+                Wishlist::create([
+                    'user_id' => auth()->user()->id,
+                    'product_id' => $productId,
+                ]);
+                session()->flash('message', 'Wishlist Added Successfully');
+            }
+        } else {
+            session()->flash('message', 'Please Login to continue');
+            return false;
+        }
+    }
 
     public function colorSelected($productColorId)
     {
